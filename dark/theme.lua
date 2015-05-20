52215a2845235a0b7d9e051f13e1bb907e890e38
Guillaume Dott <guillaume+github@dott.fr>
2015-05-20 11:50:11 +0200

    Modify border_focus color

--- dark/theme.lua
+++ dark/theme.lua
@@ -18,7 +18,7 @@ theme.fg_minimize   = "#ffffff"
 
 theme.border_width  = 1
 theme.border_normal = "#000000"
-theme.border_focus  = "#ffffff"
+theme.border_focus  = "#eeeeee"
 theme.border_marked = "#ff0000"
 
 -- There are another variables sets
